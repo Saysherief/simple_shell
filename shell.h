d5125e1fcca21b6477957ce1e8dec114d6061168
Saysherief <suhairdama@gmail.com>
2023-04-25 13:20:08 -0700

    Created header file

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void prompt(void);
+
+#endif /* SHELL_H */
