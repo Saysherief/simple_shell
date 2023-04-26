c1761ae677da61385a776090db2ab29e61750629
Umuhasna <fa1033170@gmail.com>
2023-04-26 13:25:19 +0300

    Write the basic code for simple shell

---
+++ shell.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ * main - simple UNIX command line interpreter
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char *line, *av[2];
+	int status, mode;
+	size_t size = 0;
+	ssize_t nread;
+	pid_t pid;
+
+	while (1)
+	{
+		mode = isatty(STDIN_FILENO);
+		if (mode == 1)
+			prompt();
+		line = NULL;
+		nread = getline(&line, &size, stdin);
+		if (nread == -1)
+		{
+			/*perror("getline");*/
+			exit(EXIT_FAILURE);
+		}
+		av[0] = strtok(line, " \n");
+		av[1] = NULL;
+		if (strcmp(av[0], "^D") == 0)
+		{
+			perror("");
+			exit(EXIT_SUCCESS);
+		}
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			if (execve(av[0], av, NULL) == -1)
+			{
+				perror("./hsh");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (wait(&status) == -1)
+			{
+				perror("wait");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		free(line);
+	}
+
+	return (0);
+}
