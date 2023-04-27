3e74d6aa17d0b4caf75ade196fe2b1804fed8104
Saysherief <suhairdama@gmail.com>
2023-04-26 17:40:39 -0700

    Added _strcat

--- string.c
+++ string.c
@@ -33,3 +33,27 @@ int _strlen(char *s)
 	}
 	return (len);
 }
+
+/**
+ * _strcat - concatenates the second string to the first and retuns the address
+ * @dest: address of the second string
+ * @src: address of the first string
+ * Return: address of the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
