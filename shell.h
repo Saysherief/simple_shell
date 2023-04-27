114ab68c1502b8b7490fcb8556612a466baeea79
Umuhasna <fa1033170@gmail.com>
2023-04-27 04:22:47 +0300

    Updated

--- shell.h
+++ shell.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stddef.h>
 
 #define MAX 100
 
@@ -13,8 +14,11 @@ void prompt(void);
 char **handle_args(char *lineptr);
 void handle_EOF(ssize_t nread);
 void exec_command(char **av, char **argv);
+void child_process(char **av, char **argv, int mode, pid_t p_pid);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_itoa(int num);
+void rev_string(char *s);
 
 #endif /* SHELL_H */
