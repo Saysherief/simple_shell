66f972d30bf6a60177ea8231ee11fdeb62019088
Umuhasna <fa1033170@gmail.com>
2023-04-27 04:23:26 +0300

    Fix betty error, create child_process

--- shell.c
+++ shell.c
@@ -93,9 +93,8 @@ void handle_EOF(ssize_t nread)
  */
 void exec_command(char **av, char **argv)
 {
-	int status, i, mode = isatty(STDIN_FILENO), c_line;
-	char *error_msg, *not_found_msg/*, *st_line*/;
-	pid_t p_pid = getpid(), ch_pid, pid = fork();
+	int status, mode = isatty(STDIN_FILENO);
+	pid_t p_pid = getpid(), pid = fork();
 
 	if (pid == -1)
 	{
@@ -104,36 +103,7 @@ void exec_command(char **av, char **argv)
 	}
 	else if (pid == 0)
 	{
-		ch_pid = getpid();
-		c_line = ch_pid - p_pid;
-		if (access(av[0], X_OK) == -1)
-		{
-			if (mode != 1)
-			{
-				error_msg = _strcat(argv[0], ": 1: ");
-				write(STDERR_FILENO, error_msg, _strlen(error_msg)); }
-			else
-			{
-				/*error_msg = _strcat(argv[0], ": ");
-				error_msg = _strcat(error_msg, itoa(c_line, st_line, 10));
-				error_msg = _strcat(error_msg, ": ");*/
-				printf("%s: %d: ", argv[0], c_line);
-			}
-			/*write(STDERR_FILENO, error_msg, _strlen(error_msg));*/ 
-			write(STDERR_FILENO, av[0], _strlen(av[0]));
-			not_found_msg = ": not found\n";
-			write(STDERR_FILENO, not_found_msg, _strlen(not_found_msg));
-			for (i = 0; av[i]; i++)
-				free(av[i]);
-			free(av);
-			exit(EXIT_FAILURE); }
-		if (execve(av[0], av, NULL) == -1)
-		{
-			perror(argv[0]);
-			for (i = 0; av[i]; i++)
-				free(av[i]);
-			free(av);
-			exit(EXIT_FAILURE); }
+		child_process(av, argv, mode, p_pid);
 	}
 	else
 	{
@@ -142,4 +112,49 @@ void exec_command(char **av, char **argv)
 			perror("wait");
 			exit(EXIT_FAILURE); }
 	}
+}
+
+/**
+ * child_process - the child process
+ * @av: pointer to pointer
+ * @argv: pointer to pointer
+ * @mode: return of isatty
+ * @p_pid: parent pid
+ *
+ * Return: nothing
+ */
+void child_process(char **av, char **argv, int mode, pid_t p_pid)
+{
+	int i, c_line;
+	char *st_line, *error_msg, *not_found_msg;
+	pid_t ch_pid;
+
+	ch_pid = getpid();
+	c_line = ch_pid - p_pid;
+	if (access(av[0], X_OK) == -1)
+	{
+		if (mode != 1)
+			error_msg = _strcat(argv[0], ": 1: ");
+		else
+		{
+			st_line = _itoa(c_line);
+			error_msg = _strcat(argv[0], ": ");
+			error_msg = _strcat(error_msg, st_line);
+			error_msg = _strcat(error_msg, ": ");
+		}
+		write(STDERR_FILENO, error_msg, _strlen(error_msg));
+		write(STDERR_FILENO, av[0], _strlen(av[0]));
+		not_found_msg = ": not found\n";
+		write(STDERR_FILENO, not_found_msg, _strlen(not_found_msg));
+		for (i = 0; av[i]; i++)
+			free(av[i]);
+		free(av);
+		exit(EXIT_FAILURE); }
+	if (execve(av[0], av, NULL) == -1)
+	{
+		perror(argv[0]);
+		for (i = 0; av[i]; i++)
+			free(av[i]);
+		free(av);
+		exit(EXIT_FAILURE); }
 }
